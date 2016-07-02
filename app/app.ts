01f696a6591fec903ae0fca3a4932f0dd29ac9e6
Miller P. Magalhães <miller@mdpdev.com.br>
2016-07-02 10:11:39 -0300

    up

---
+++ app/app.ts
@@ -0,0 +1,31 @@
+import {Component} from '@angular/core';
+import {Platform, ionicBootstrap} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+import {TabsPage} from './pages/tabs/tabs';
+import {BarcodeScanner} from 'ionic-native';
+
+@Component({
+    template: '<ion-nav [root]="rootPage"></ion-nav>'
+})
+export class MyApp {
+
+    private rootPage:any;
+
+    constructor(private platform:Platform) {
+        this.rootPage = TabsPage;
+
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            StatusBar.styleDefault();
+        });
+    }
+}
+
+ionicBootstrap(MyApp)
+
+BarcodeScanner.scan().then((barcodeData) => {
+    console.log(barcodeData);
+}, (err) => {
+    console.log(err);
+});
