615874b0efaccac333b93b61226fed75bcf038c0
Miller P. Magalhães <miller@mpdev.com.br>
2016-07-04 17:20:46 -0300

    testes

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
-import {TabsPage} from './pages/tabs/tabs';
+import {ionicBootstrap} from 'ionic-angular';
+import {HomePage} from './pages/home/home';
 
 @Component({
     template: '<ion-nav [root]="rootPage"></ion-nav>'
@@ -10,17 +9,8 @@ export class MyApp {
 
     private rootPage:any;
 
-    constructor(private platform:Platform) {
-        this.rootPage = TabsPage;
-
-        platform.ready().then(() => {
-            // Okay, so the platform is ready and our plugins are available.
-            // Here you can do any higher level native things you might need.
-            StatusBar.styleDefault();
-        });
-
+    constructor() {
+        this.rootPage = HomePage;
     }
 }
-
-ionicBootstrap(MyApp)
-
+ionicBootstrap(MyApp);
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,26 +1,65 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {BarcodeScanner} from 'ionic-native';
+import {Http} from '@angular/http';
+import 'rxjs/Rx';
+import {Vibration} from 'ionic-native';
+import {Screenshot} from 'ionic-native';
+import {Flashlight} from 'ionic-native';
+import {SocialSharing} from 'ionic-native';
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+    templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
 
-  code = '123';
+    code = '';
+    posts;
+    customers;
+    postsUrl = 'http://172.20.253.99:8000/api/v1/posts?api_token=123';
+    customersApi = 'http://172.20.253.99:8000/api/v1/customers?api_token=123';
 
-  constructor(private navController: NavController) {
-  
-  }
+    constructor(private navController:NavController, private http:Http) {
+        this.http.get(this.customersApi)
+            .map(res => res.json())
+            .subscribe(customers => this.customers = customers.data);
 
-  barcodeopen() {
-    BarcodeScanner.scan().then((barcodeData) => {
-      this.code = barcodeData.text;
-    }, (err) => {
-      console.log('erro');
-      this.code = 'erro';
-    });
+        this.http.get(this.postsUrl)
+            .map(res => res.json())
+            .subscribe(posts => this.posts = posts.data.data);
+    }
 
-  }
+    barcodeopen() {
+        BarcodeScanner.scan().then((barcodeData) => {
+            this.code = barcodeData.text;
+        }, (err) => {
+            this.code = 'erro';
+        });
+
+    }
+    
+    vibrar() {
+        Vibration.vibrate(1000);
+    }
+
+    vibrarr() {
+        Vibration.vibrate([2000, 1000, 2000]);
+    }
+
+    pararv() {
+        Vibration.vibrate(0);
+    }
+
+    printS() {
+        Screenshot.save('jpg', 90);
+    }
+
+    ligarFlash(){
+        Flashlight.toggle();
+    }
+
+    share(){
+        SocialSharing.share('Mensagem Teste','Assunto Qualquer', null, 'http://mpdev.com.br');
+    }
 
 }
